6fdefa53746728fd4093f707f0447ef4c91622e5
CalvinHenslerDS <CalvinHenslerDS@gmail.com>
2025-11-22 21:25:18 -0500

    Complete one Python exercise

---
+++ w3resource/CodEx_w3_PyBas1_E51.py
@@ -0,0 +1,29 @@
+# w3resource
+
+# Python Basic Part -I
+
+
+# Exercise 50:
+
+
+# Problem Statement:
+
+# Write a Python program to determine the profiling of Python programs.
+
+
+# Solution Attempt:
+
+import cProfile
+
+# Define an example function
+def sum():
+    print(1 + 2)
+
+# Use cProfile to profile the sum function
+cProfile.run('sum()')
+
+
+# Lessons Learned:
+
+# Modules:
+# cProfile: Built-in module designed for profile the performance of Python code
