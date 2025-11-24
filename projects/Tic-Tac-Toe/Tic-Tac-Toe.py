4acf1f4672ef2fbd4fada2c19253e957f6bd372f
CalvinHenslerDS <CalvinHenslerDS@gmail.com>
2025-11-24 17:45:43 -0500

    Bug fix check_win() and continue commenting in Tic-Tac-Toe.py

--- projects/Tic-Tac-Toe/Tic-Tac-Toe.py
+++ projects/Tic-Tac-Toe/Tic-Tac-Toe.py
@@ -8,11 +8,11 @@ def coin_flip():
     # Prompt an input from the user, which will be used to determine whether the user or the computer will move first
     heads_tails = input("Select Heads or Tails:")
 
-    # Generate a random number (1 or 2) to simulate the result of a coin flip
+    # Generate a random integer (1 or 2) to simulate the result of a coin flip
     flip_result = random.randint(1,2)
 
     # Initialize a boolean to track whether it is currently the user's or the computer's turn to play
-    computer_first = None
+    computer_move = None
 
     # Convert heads_tales to an integer (1 or 2) to compare to flip_result
     if heads_tails == "Heads":
@@ -20,21 +20,22 @@ def coin_flip():
     else:
         heads_tails = 2
 
-    # Indicate to the user the result of the coin flip
+    # Print the result of the coin flip
     if flip_result == 1:
         print("The coin landed on Heads.")
     else:
         print("The coin landed on Tails.")
 
-    # Indicate to the user whether the user or the computer will play first
+    # Print who will play move
     if flip_result == heads_tails:
-        computer_first = False
+        computer_move = False
         print("You go first!")
     else:
-        computer_first = True
+        computer_move = True
         print("The computer goes first.")
 
-    return computer_first
+    # Return computer_move for use in play_game
+    return computer_move
 
 def random_index_from_mask(board):
 
@@ -47,60 +48,91 @@ def random_index_from_mask(board):
     # Randomly choose an index from candidate_indices
     random_index_in_candidates = np.random.choice(len(candidate_indices[0]))
 
+    # Return a tuple that consists of the random_index_in-candidates-th term of each NumPy array in the candidate_indices tuple
     return tuple(index[random_index_in_candidates] for index in candidate_indices)
 
-def make_move(board, computer_first):
+def make_move(board, computer_move):
 
-    if computer_first == True:
-        affected_square = random_index_from_mask(board)
-        board[affected_square] = 1
+    # If it is the computer's move, change a randomly-selected item in board from 0 to 1
+    if computer_move == True:
+        computer_move_coordinates = random_index_from_mask(board)
+        board[computer_move_coordinates] = 1
+
+    # Otherwise, prompt the user to input the row and column of an item in board that is currently equal to 0, and change it to 2
     else:
         player_move_coordinates = input("Select an empty square to place an 'O' in: (format: row#,column#)")
         player_move_coordinates_int_list = list(map(int, player_move_coordinates.split(',')))
-        # Insert error handling for invalid player move
+
+        # Initialize a boolean to facilitate error handling if an invalid input is given
+        valid = False
+
+        # Initialize a while loop to iterate until the input is valid
+        while valid == False:
+            
+            # If the input corresponds to coordinates of an item in board that is equal to 0, toggle valid to break from the while loop
+            if board[player_move_coordinates_int_list[0]][player_move_coordinates_int_list[1]] == 0:
+                valid = True
+
+            # If the input does not correspond to coordinates of an item in board that is equal to 0, prompt the user for a valid input and repeat the validation check
+            else:
+                print("That input is invalid.")
+                player_move_coordinates = input("Select an empty square to place an 'O' in: (format: row#,column#)")
+                player_move_coordinates_int_list = list(map(int, player_move_coordinates.split(',')))
+
+        # Update board with the user's valid input
         board[player_move_coordinates_int_list[0], player_move_coordinates_int_list[1]] = 2
-    computer_first = not computer_first
-    return board, computer_first
+    
+    # Toggling computer_move to ensure the opposite party moves when make_move is called again
+    computer_move = not computer_move
 
-
+    # Return the current board state and computer_move
+    return board, computer_move
 
 def win_check(board):
-#print(len(set(board[0])))
-#print(board[0])
-#print(board[:,0])
+
+    # Initialize booleans to track whether the computer or the user has satisfied a win condition
     computer_winner = False
     challenger_winner = False
+
+    # Initialize empty lists to store the lengths of the sets of the rows and columns of board
     set_lengths_rows = []
     set_lengths_columns = []
 
+    # Calculate the length of the set of the diagonal and antidiagonal of board
     set_length_diag = len(set(np.diag(board)))
-    sum_diag = sum(np.diag(board))
-    #print(set_length_diag)
-    #print(sum_diag)
     set_length_antidiag = len(set(np.diag(np.fliplr(board))))
+
+    # Calculate the sum of the diagonal and antidiagonal of board
+    sum_diag = sum(np.diag(board))
     sum_antidiag = sum(np.diag(np.fliplr(board)))
-    # print(set_length_antidiag)
-    # print(sum_antidiag)
+    
     
     for i in board:
+
+        # Append the length of the set of each row to set_lengths_rows
         set_lengths_rows.append(len(set(i)))
-        sum_rows = sum(board)
-    
-    sum_rows_list = sum_rows.tolist()
 
-    # print(set_lengths_rows)
-    # print(sum_rows)
-    # print(sum_rows_list)
+        # Create an array comprised of the sums of the items in the rows of board
+        sum_rows = np.sum(board, axis=1)
 
+        # Create an array comprised of the sums of the items in the columns of the board
+        sum_columns = np.sum(board, axis=0)
+
+    # Append the length of the set of each column to set_lengths_columns
     for i in board.T:
-        set_lengths_columns.append(len(set(i)))
-        sum_columns = sum(board.T)
+         set_lengths_columns.append(len(set(i)))
+
+    # Convert sum_rows and sum_columns to lists
+    sum_rows_list = sum_rows.tolist()
+    sum_columns_list = sum_columns.tolist()
+
+
 
     sum_columns_list = sum_columns.tolist()
 
-    # print(set_lengths_columns)
-    # print(sum_columns)
-    # print(sum_columns_list)
+    print(set_lengths_columns)
+    print(sum_columns)
+    print(sum_columns_list)
 
     if set_length_diag == 1:
         if sum_diag == 3:
@@ -127,11 +159,11 @@ def win_check(board):
             pass
 
     for i in set_lengths_columns:
-        if set_lengths_columns == 1 and sum_columns_list == 3:
+        if set_lengths_columns[i] == 1 and sum_columns_list[i] == 3:
             computer_winner = True
             print(board, "The computer won along column %i" % i)
             return computer_winner, challenger_winner
-        elif set_lengths_columns == 1 and sum_columns_list == 6:
+        elif set_lengths_columns[i] == 1 and sum_columns_list[i] == 6:
             challenger_winner = True
             print(board, "The challenger won along column %i" % i)
             return computer_winner, challenger_winner
@@ -139,11 +171,11 @@ def win_check(board):
             continue
 
     for i in set_lengths_rows:
-        if set_lengths_rows == 1 and sum_rows_list == 3:
+        if set_lengths_rows[i] == 1 and sum_rows_list[i] == 3:
             computer_winner = True
             print(board, "The computer won along column %i" % i)
             return computer_winner, challenger_winner
-        elif set_lengths_rows == 1 and sum_rows_list == 6:
+        elif set_lengths_rows[i] == 1 and sum_rows_list[i] == 6:
             challenger_winner = True
             print(board, "The challenger won along column %i" % i)
             return computer_winner, challenger_winner
@@ -152,11 +184,11 @@ def win_check(board):
 
 def play_game():
     board = np.zeros((3,3))
-    computer_first = coin_flip()
+    computer_move = coin_flip()
     
     for i in range(10):
         print(board)
-        board, computer_first = make_move(board, computer_first)
+        board, computer_move = make_move(board, computer_move)
         computer_winner, challenger_winner = win_check(board)
         if (computer_winner or challenger_winner) == True:
             return computer_winner, challenger_winner, board
@@ -164,7 +196,6 @@ def play_game():
             continue
 
 play_game()
-
 
 '''
     if computer_turn == True:
