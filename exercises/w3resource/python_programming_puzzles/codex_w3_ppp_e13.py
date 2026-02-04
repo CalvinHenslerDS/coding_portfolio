fc4ea0d735f077257ce1f74e5d9c34316f4304ee
CalvinHenslerDS <CalvinHenslerDS@gmail.com>
2026-02-04 08:36:54 -0500

    Complete one python exercise

--- exercises/w3resource/python_programming_puzzles/codex_w3_ppp_e13.py
+++ exercises/w3resource/python_programming_puzzles/codex_w3_ppp_e13.py
@@ -11,11 +11,11 @@ w3resource
 
 Write a Python program to find strings in a given list starting with a given prefix.
 Input:
-[( ca,('cat', 'car', 'fear', 'center'))]
+[( 'ca',('cat', 'car', 'fear', 'center'))]
 Output:
 ['cat', 'car']
 Input:
-[(do,('cat', 'dog', 'shatter', 'donut', 'at', 'todo'))]
+[('do',('cat', 'dog', 'shatter', 'donut', 'at', 'todo'))]
 Output:
 ['dog', 'donut']
 
@@ -24,16 +24,11 @@ Output:
 
 def prefix_checker(input_list):
     prefix = input_list[0][0]
-    print(prefix)
     prefix_length = len(prefix)
     output_list = []
     for element in input_list[0][1]:
-        element_length = len(element)
-        element_prefix_delta = element_length - prefix_length
-        print(element_prefix_delta)
-        not_prefix = element[:element_prefix_delta]
-        print(not_prefix)
-        if element[prefix_length:] == prefix:
+        element_prefix = element[:prefix_length]
+        if element_prefix == prefix:
             output_list.append(element)
 
     return output_list
