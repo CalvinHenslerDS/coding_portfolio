46e9a7d628cbd480d018e79141179cbcf65c6466
CalvinHenslerDS <CalvinHenslerDS@gmail.com>
2026-02-11 09:54:36 -0500

    Complete one python exercise

--- exercises/leetcode/leetcode_75/lc75_1071.py
+++ exercises/leetcode/leetcode_75/lc75_1071.py
@@ -46,69 +46,25 @@ class Solution(object):
         
         output = ""
         working_divisor = ""
-        candidate_divisors = []
+        divisor = ""
         length_str1 = len(str1)
         length_str2 = len(str2)
 
         for letter in str1:
             working_divisor += letter
-            candidate_divisors.append(working_divisor)
-        
-        for candidate in candidate_divisors:
-            candidate_length = len(candidate)
-            
+            length_working_divisor = len(working_divisor)
 
-#         output = ""
-#         candidate_divisor = ""
-#         candidate_divisors = []
-#         divisor = ""
-#         length1 = len(str1)
-#         length2 = len(str2)
-        
-#         for letter1 in str1:
-            
-#             print(candidate_divisor)
-#             if (candidate_divisor[0] == letter1) or (len(candidate_divisor) == len(str1)):
-#                 length_divisor = len(candidate_divisor)
-#                 print(length_divisor)
-#                 remainder1 = length1 % length_divisor
+            if (length_str1 % len(working_divisor) == 0) and (length_str2 % len(working_divisor) == 0):
+                validator1 = int(length_str1 / length_working_divisor) * working_divisor
+                validator2 = int(length_str2 / length_working_divisor) * working_divisor
+                if (validator1 == str1) and (validator2 == str2):
+                    divisor = working_divisor
 
-#                 if remainder1 == 0:
-#                     print("here")
-#                     candidate_repeat_count1 = int(length1 / length_divisor)
-#                     #print(candidate_repeat_count1)
-
-#                     if candidate_divisor * candidate_repeat_count1 == str1:
-#                         candidate_repeat_count2 = int(length2 / length_divisor)
-#                         #print(candidate_divisor)
-#                         if candidate_repeat_count2 * candidate_divisor == str2:
-#                             candidate_divisors.append(candidate_divisor)
-#                             #print("hi")
-#                             for i in range(candidate_repeat_count1):
-#                                 candidate_divisor = (i + 1) * candidate_divisor
-#                                 length_candidate_divisor = len(candidate_divisor)
-#                                 candidate_repeat_count1 = int(length1 / length_candidate_divisor)
-#                                 candidate_repeat_count2 = int(length2 / length_candidate_divisor)
-#                                 validator1 = candidate_repeat_count1 * candidate_divisor
-#                                 validator2 = candidate_repeat_count2 * candidate_divisor
-
-#                                 if (validator1 == str1) and (validator2 == str2):
-#                                     divisor = candidate_divisor
+        return divisor
 
 
-#                         else:
-#                             print(candidate_divisors)
-#                             divisor = ""
-
-#                         return divisor
-            
-            
-
-
-
-# sol = Solution()
-# print(sol.gcdOfStrings('LEETLEETLEET','LEETLEET'))
-
-#print(f"inputs: 'abc', 'pqr': {sol.mergeAlternately('abc','pqr')}")
-#print(f"inputs: 'ab', 'pqrs': {sol.mergeAlternately('ab','pqrs')}")
-#print(f"inputs: 'abcd', 'pq': {sol.mergeAlternately('abcd','pq')}")
+sol = Solution()
+print(sol.gcdOfStrings('ABCABC','ABC'))
+print(sol.gcdOfStrings('ABABAB','ABAB'))
+print(sol.gcdOfStrings('LEET','CODE'))
+print(sol.gcdOfStrings('AAAAAB','AAA'))
